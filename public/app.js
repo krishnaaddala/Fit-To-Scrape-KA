43b4be0ad61aa49439ca67c713fada6c221e1319
Krishna <kaddala@salesforce.com>
2020-02-27 17:20:04 -0800

    modal added for note

--- public/app.js
+++ public/app.js
@@ -86,37 +86,41 @@ $('#showModal').click(function(){
         window.location.reload();     
      });
   });
+
+  $(document).on("click", "#createMyNote", function() {
+    $("#modalNote").modal("show")
+  });
   
   // When you click the savenote button
-  $(document).on("click", "#createMyNote", function() {
+//   $(document).on("click", "#createMyNote", function() {
 
-    // 
+//     // 
 
 
-    // Grab the id associated with the article from the submit button
-    var thisId = $(this).attr("data-id");
+//     // Grab the id associated with the article from the submit button
+//     var thisId = $(this).attr("data-id");
   
-    // Run a POST request to change the note, using what's entered in the inputs
-    $.ajax({
-      method: "POST",
-      url: "/quotes/" + thisId,
-      data: {
-        // Value taken from title input
-        // title: $("#titleinput").val(),
-        // Value taken from note textarea
-        body: $("#noteText").val()
-      }
-    })
-      // With that done
-      .then(function(data) {
-        // Log the response
-        console.log(data);
-        // Empty the notes section
-        $("#notes").empty();
-      });
+//     // Run a POST request to change the note, using what's entered in the inputs
+//     $.ajax({
+//       method: "POST",
+//       url: "/quotes/" + thisId,
+//       data: {
+//         // Value taken from title input
+//         // title: $("#titleinput").val(),
+//         // Value taken from note textarea
+//         body: $("#noteText").val()
+//       }
+//     })
+//       // With that done
+//       .then(function(data) {
+//         // Log the response
+//         console.log(data);
+//         // Empty the notes section
+//         $("#notes").empty();
+//       });
   
-    // Also, remove the values entered in the input and textarea for note entry
-    $("#titleinput").val("");
-    $("#bodyinput").val("");
-  });
+//     // Also, remove the values entered in the input and textarea for note entry
+//     $("#titleinput").val("");
+//     $("#bodyinput").val("");
+//   });
   
