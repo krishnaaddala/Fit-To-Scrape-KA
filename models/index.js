9f7da1c1c82cd2a8f0eec811f449920dce78d369
Krishna <kaddala@salesforce.com>
2020-03-17 20:28:32 -0700

    creating a secondary repo to test heroku deploy

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-    // Quote: require("./quote"),
-    MOOote: require("./note")
+    Quote: require("./quote"),
+    Note: require("./note")
   };
   
